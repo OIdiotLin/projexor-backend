97a5f95cf1c3f7fe98cd92f0874b0e459861eedc
oidiotlin <oidiotlin@gmail.com>
2018-11-28 12:00:25 +0800

    用户相关 api

--- apis/urls.py
+++ apis/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from apis.views import user, project
 
 urlpatterns = [
-    path('users/', user.get_all, name='user-get-all'),
+    path('users/', user.get_list, name='user-get-list'),
     path('users/register/', user.register, name='user-register'),
     path('users/login/', user.login, name='user-login'),
+    path('users/<id>/', user.get, name='user-get'),
 
-    path('projects/', project.get_all, name='project-get-all')
+    path('projects/', project.get_list, name='project-get-list')
 ]
